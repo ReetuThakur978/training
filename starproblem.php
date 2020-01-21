fdbbe202e75949ce6e54a9ad06e5c4afc737e881
reetuthakur <reetuthakur.zapbuild@gmail.com>
2020-01-21 17:06:36 +0530

    Change in the files

---
+++ starproblem.php
@@ -0,0 +1,19 @@
+<?php
+echo "enter the valur of n="
+for(i=1; i<=n; i++)
+{
+	for(j=1; j<=n*2; j++)
+	{
+		if(j<=n+1-i)&&(j<=n-1+1)
+		{
+			echo "*";
+			k=0
+
+		}
+		else{
+			echo" ";
+			k=1;
+		}
+	}
+}
+?>
